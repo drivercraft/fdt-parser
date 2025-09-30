600c383bf2847bbac89ac3dc155efdfaea2307f8
周睿 <zrufo747@outlook.com>
2025-09-30 10:01:04 +0800

    添加对 orangepi5plus.dtb 的支持

--- gen_dts.sh
+++ gen_dts.sh
@@ -1,3 +1,4 @@
 dtc -O dts -I dtb -o target/phytium.dts dtb-file/src/dtb/phytium.dtb
 dtc -O dts -I dtb -o target/bcm2711-rpi-4-b.dts dtb-file/src/dtb/bcm2711-rpi-4-b.dtb
 dtc -O dts -I dtb -o target/rk3568-firefly-roc-pc-se.dts dtb-file/src/dtb/rk3568-firefly-roc-pc-se.dtb
+dtc -O dts -I dtb -o target/orangepi5plus.dts dtb-file/src/dtb/orangepi5plus.dtb
